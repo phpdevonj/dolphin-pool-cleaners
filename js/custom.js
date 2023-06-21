9887e445cf92c8c04d09e7c47fca891b97eb6de4
hitesh kumar <phpdevelopment@ongraph.com>
2023-06-21 22:25:25 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,195 @@
+$(document).ready(function () {
+  $(".values li").click(function () {
+    $(".values li").removeClass("active");
+    $(this).addClass("active");
+    $(this)
+      .closest(".wizard-fieldset")
+      .find(".form-wizard-next-btn")
+      .removeClass("disable");
+    $(this)
+      .closest(".wizard-fieldset")
+      .find(".form-wizard-submit")
+      .removeClass("disable");
+  });
+  // next button
+  $(".form-wizard-next-btn,.form-wizard-submit").click(function () {
+    var currentFieldset = $(this).closest(".wizard-fieldset");
+    currentFieldset.removeClass("show");
+    currentFieldset.next(".wizard-fieldset").addClass("show");
+  });
+  $(".form-wizard-submit").click(function () {
+    $(".form-wizard-header").addClass("d-none");
+  });
+  // previous button
+  $(".form-wizard-previous-btn").click(function () {
+    var currentFieldset = $(this).closest(".wizard-fieldset");
+    currentFieldset.removeClass("show");
+    currentFieldset.prev(".wizard-fieldset").addClass("show");
+  });
+
+  $(".round_rectangle .tab_btn li").click(function () {
+    // Remove 'active' class from all images
+    $(".round_rectangle .card-default-image img").removeClass("active");
+    // Get the class of the clicked button's parent li
+    var selectedShape = $(this).attr("class");
+    // Add 'active' class to the corresponding image
+    $(".round_rectangle .card-default-image img." + selectedShape).addClass(
+      "active"
+    );
+    $(this)
+      .closest(".wizard-fieldset")
+      .find(".form-wizard-next-btn")
+      .removeClass("disable");
+  });
+  $(".floor_wall .tab_btn li").click(function () {
+    // Remove 'active' class from all images
+    $(".floor_wall .card-default-image img").removeClass("active");
+    // Get the class of the clicked button's parent li
+    var selectedShape = $(this).attr("class");
+    // Add 'active' class to the corresponding image
+    $(".floor_wall .card-default-image img." + selectedShape).addClass(
+      "active"
+    );
+    $(this)
+      .closest(".wizard-fieldset")
+      .find(".form-wizard-next-btn")
+      .removeClass("disable");
+  });
+  $(".tab_btn li").click(function () {
+    $(".tab_btn li").removeClass("active"); // Remove active class from all li elements
+    $(this).addClass("active"); // Add active class to the clicked li element
+  });
+  //   range slider
+  var valMap = ["0", "30", "33", "40", "50"];
+  var slider = $("#resolution-slider");
+
+  slider.slider({
+    value: 0,
+    max: valMap.length - 1,
+    min: 0,
+    create: function (event, ui) {
+      $(".resolution-preview").html(valMap[0]);
+      updateNextButtonClass(valMap[0]);
+    },
+    slide: function (event, ui) {
+      var sliderWidth = (ui.value / (valMap.length - 1)) * 100 + "%";
+      $(".resolution-preview").html(valMap[ui.value]);
+      $(".range_slider_count span").html(valMap[ui.value] + "ft.");
+      $(".ui-slider-range").css("width", sliderWidth);
+      updateNextButtonClass(valMap[ui.value]);
+    },
+  });
+  function updateNextButtonClass(value) {
+    var nextButton = $(".form-wizard-next-btn");
+    if (value === "0") {
+      nextButton.addClass("disable");
+    } else {
+      nextButton.removeClass("disable");
+    }
+  }
+
+  // checkbox change
+  $(".checkbox").change(function () {
+    $(".range_slider_count span").removeClass("d-none");
+    $(".metrics-images .card-default-image img").removeClass("active");
+    if ($(this).is(":checked")) {
+      $(".metrics-images .card-default-image .round_class").addClass("active");
+      $(".range_slider_count p").html("Diameter up to");
+    } else {
+      $(".metrics-images .card-default-image .rect_class").addClass("active");
+      $(".range_slider_count p").html("Length up to");
+    }
+  });
+
+  // click on next button
+  $(".form-wizard-next-btn").click(function () {
+    var parentFieldset = $(this).parents(".wizard-fieldset");
+    var currentActiveStep = $(this)
+      .parents(".form-wizard")
+      .find(".form-wizard-steps .active");
+    var next = $(this);
+    var nextWizardStep = true;
+    parentFieldset.find(".wizard-required").each(function () {
+      var thisValue = $(this).val();
+
+      if (thisValue == "") {
+        $(this).siblings(".wizard-form-error").slideDown();
+        nextWizardStep = false;
+      } else {
+        $(this).siblings(".wizard-form-error").slideUp();
+      }
+    });
+    if (nextWizardStep) {
+      next.parents(".wizard-fieldset").removeClass("show", "400");
+      currentActiveStep
+        .removeClass("active")
+        .addClass("activated")
+        .next()
+        .addClass("active", "400");
+      next
+        .parents(".wizard-fieldset")
+        .next(".wizard-fieldset")
+        .addClass("show", "400");
+      $(document)
+        .find(".wizard-fieldset")
+        .each(function () {
+          if ($(this).hasClass("show")) {
+            var formAtrr = $(this).attr("data-tab-content");
+            $(document)
+              .find(".form-wizard-steps .form-wizard-step-item")
+              .each(function () {
+                if ($(this).attr("data-attr") == formAtrr) {
+                  $(this).addClass("active");
+                  var innerWidth = $(this).innerWidth();
+                  var position = $(this).position();
+                  $(document)
+                    .find(".form-wizard-step-move")
+                    .css({ left: position.left, width: innerWidth });
+                } else {
+                  $(this).removeClass("active");
+                }
+              });
+          }
+        });
+    }
+  });
+  //click on previous button
+  $(".form-wizard-previous-btn").click(function () {
+    var counter = parseInt($(".wizard-counter").text());
+    var prev = $(this);
+    var currentActiveStep = $(this)
+      .parents(".form-wizard")
+      .find(".form-wizard-steps .active");
+    prev.parents(".wizard-fieldset").removeClass("show", "400");
+    prev
+      .parents(".wizard-fieldset")
+      .prev(".wizard-fieldset")
+      .addClass("show", "400");
+    currentActiveStep
+      .removeClass("active")
+      .prev()
+      .removeClass("activated")
+      .addClass("active", "400");
+    $(document)
+      .find(".wizard-fieldset")
+      .each(function () {
+        if ($(this).hasClass("show")) {
+          var formAtrr = $(this).attr("data-tab-content");
+          $(document)
+            .find(".form-wizard-steps .form-wizard-step-item")
+            .each(function () {
+              if ($(this).attr("data-attr") == formAtrr) {
+                $(this).addClass("active");
+                var innerWidth = $(this).innerWidth();
+                var position = $(this).position();
+                $(document)
+                  .find(".form-wizard-step-move")
+                  .css({ left: position.left, width: innerWidth });
+              } else {
+                $(this).removeClass("active");
+              }
+            });
+        }
+      });
+  });
+});
---
+++ js/main.js
@@ -0,0 +1,54 @@
+var pool_type = '';
+var floor_type = '';
+var pool_shape = '';
+var dirt_and_debris = '';
+var dimention = '';
+const poolType = (obj) => {pool_type = $(obj).attr('data-attr');}
+const floorType = (arg) => {floor_type = arg;}
+const shapePool = (arg) => {pool_shape = arg;}
+const dirtAndDebris = (arg) => {dirt_and_debris = arg;}
+const getDimentions = (obj) => {
+    dimention = $("#resolution-preview").text(); 
+}
+
+$("#submitForm").on('click',(e) => {
+        e.preventDefault();
+        $("#poolType").html(pool_type);
+        $("#poolShape").html(floor_type);
+        $("#poolCoverage").html(pool_shape);
+        $("#poolControl").html(dirt_and_debris);
+        $("#poolSize").html(dimention);
+
+        if(pool_type == "Inground" && (floor_type == "Floor" || floor_type == "Floor and Walls" || floor_type == "Floor, Walls and Waterline") && (dirt_and_debris == "Large" || dirt_and_debris == "Small")){
+            $(".dolphin_premier").removeClass("product");
+            $(".dolphin_premier").find(".dolphin_premier_img").attr("src","Images/premier.jpg");
+
+            $(".dolphin_sigma").removeClass("product");
+            $(".dolphin_sigma").find(".dolphin_sigma_img").attr("src","Images/sigma.jpg");
+
+            $(".dolphin_quantum").removeClass("product");
+            $(".dolphin_quantum").find(".dolphin_quantum_img").attr("src","Images/quantum.jpg");
+        }
+
+        if(pool_type == "Above ground" && (floor_type == "Floor" || floor_type == "Floor and Walls" || floor_type == "Floor, Walls and Waterline") && (dirt_and_debris == "Large" || dirt_and_debris == "Small")){
+
+            $(".dolphin_quantum").removeClass("product");
+            $(".dolphin_quantum").find(".dolphin_quantum_img").attr("src","Images/quantum.jpg");
+        }
+
+        if(pool_type == "Inground" && (floor_type == "Floor" || floor_type == "Floor and Walls") && dirt_and_debris == "Large"){
+
+            $(".dolphin_cayman").removeClass("product");
+            $(".dolphin_cayman").find(".dolphin_cayman_img").attr("src","Images/cayman.jpg");
+        }
+
+        if(pool_type == "Above ground" && (floor_type == "Floor")){
+
+            $(".dolphin_escape").removeClass("product");
+            $(".dolphin_escape").find(".dolphin_escape_img").attr("src","Images/escape.jpg");
+        }
+
+        $('#product_list').find('.product').hide();
+});
+
+
